20d37fa8f551a40ddee5a49bf423bf020ee05cc8
=Matthis <matthis.auvinet@reseau.eseo.fr>
2025-11-13 16:19:40 +0100

    Matrix/Roundcube création des comptes !

--- backend/subscribe.go
+++ backend/subscribe.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
 	"net/http"
+	"os/exec"
 	"strings"
 	"time"
 
@@ -31,6 +34,48 @@ type Connecti struct {
 	Password  string `json:"password"`
 	LastLogin string `json:"lastlogin"`
 }
+
+// Créer un compte mail dans le mailserver
+func createMailAccount(username, password string) error {
+	email := username + "@office1789.local"
+	
+	// Commande Docker pour créer le compte mail
+	cmd := exec.Command("docker", "exec", "mailserver", "setup", "email", "add", email, password)
+	
+	var stderr bytes.Buffer
+	cmd.Stderr = &stderr
+	
+	err := cmd.Run()
+	if err != nil {
+		return fmt.Errorf("failed to create mail account: %v - %s", err, stderr.String())
+	}
+	
+	return nil
+}
+
+// Créer un compte Matrix (Synapse)
+func createMatrixAccount(username, password string) error {
+	// Utiliser register_new_matrix_user CLI tool de Synapse
+	// Cette commande crée un utilisateur via l'interface en ligne de commande
+	cmd := exec.Command("docker", "exec", "synapse", 
+		"register_new_matrix_user", 
+		"http://localhost:8008",
+		"-u", username,
+		"-p", password,
+		"-c", "/data/homeserver.yaml",
+		"--no-admin")
+	
+	var stderr bytes.Buffer
+	cmd.Stderr = &stderr
+	
+	err := cmd.Run()
+	if err != nil {
+		return fmt.Errorf("failed to create matrix account: %v - %s", err, stderr.String())
+	}
+	
+	return nil
+}
+
 
 func Sub(c *gin.Context) {
 
@@ -85,6 +130,8 @@ func Sub(c *gin.Context) {
 		return
 	}
 
+	// Garder le mot de passe en clair pour créer les comptes mail/Matrix
+	plainPassword := subi.Password
 	subi.Password = HashPassword(subi.Password)
 
 	var userID int
@@ -95,6 +142,26 @@ func Sub(c *gin.Context) {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create user"})
 		return
 	}
+
+	// Créer le compte mail automatiquement
+	go func() {
+		err := createMailAccount(subi.Username, plainPassword)
+		if err != nil {
+			fmt.Printf("Warning: Failed to create mail account for %s: %v\n", subi.Username, err)
+		} else {
+			fmt.Printf("Mail account created for %s@office1789.local\n", subi.Username)
+		}
+	}()
+
+	// Créer le compte Matrix automatiquement
+	go func() {
+		err := createMatrixAccount(subi.Username, plainPassword)
+		if err != nil {
+			fmt.Printf("Warning: Failed to create Matrix account for %s: %v\n", subi.Username, err)
+		} else {
+			fmt.Printf("Matrix account created for @%s:office1789.com\n", subi.Username)
+		}
+	}()
 
 	sessionToken := uuid.NewString()
 	expiresAtTime := time.Now().Add(24 * time.Hour)
